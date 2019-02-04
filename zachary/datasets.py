08ca91622662e7bb72b571ecc6a410890bce5159
kureta <skureta@gmail.com>
2019-02-04 13:47:23 +0300

    Back to the future

--- zachary/datasets.py
+++ zachary/datasets.py
@@ -1,15 +1,11 @@
-import os
 from multiprocessing import Pool
 
+import librosa
+import librosa.effects
+import librosa.util
+import numpy as np
 import torch
 from torch.utils.data import Dataset
-
-import numpy as np
-
-from librosa import load
-from librosa import stft, istft
-from librosa.effects import trim
-from librosa.util import normalize
 
 # DEFAULT_DIR = "/home/kureta/Music/Billboard Hot 100 Singles Charts/" \
 #               "Billboard Hot 100 Singles Chart (03.03.2018) Mp3 (320kbps) [Hunter]"
@@ -18,129 +14,65 @@ FRAME_LENGTH = 1024
 HOP_LENGTH = 128
 
 
-def recursive_file_paths(directory):
-    file_paths = []
-    for dirname, dirnames, filenames in os.walk(directory):
-        # print path to all filenames.
-        for filename in filenames:
-            if filename.endswith(".mp3"):
-                file_paths.append(os.path.join(dirname, filename))
-
-    return file_paths
+def do_multiprocess(function, args_list, num_processes=8):
+    with Pool(num_processes) as p:
+        results = list(p.map(function, args_list))
+    return results
 
 
-def complex_stft(x):
-    # c_stft = stft(x, n_fft=FRAME_LENGTH, hop_length=HOP_LENGTH, center=False, pad_mode='none')
-    c_stft = stft(x, n_fft=FRAME_LENGTH, hop_length=HOP_LENGTH)
-    magnitude = np.abs(c_stft)
-    phase = np.angle(c_stft)
-
-    # dims = (channels, duration, mag/phase)
-    return np.concatenate((np.expand_dims(magnitude, 2), np.expand_dims(phase, 2)), axis=2)
-
-
-def istft_(x):
-    return istft(x[:, :, 0] * np.exp(1j * x[:, :, 1]), hop_length=512, win_length=1024)
-
-
-def load_audio_file(path):
-    audio, _ = load(path, sr=44100, mono=True)
-    audio = normalize(audio)
-    audio, _ = trim(audio, top_db=40, ref=np.max, frame_length=FRAME_LENGTH, hop_length=HOP_LENGTH)
-    padding = len(audio) % HOP_LENGTH
-    audio = np.pad(audio, (0, padding), 'constant')
-    # audio = complex_stft(audio)
-
+def load_audio_file(path, sr=44100, mono=True):
+    audio, _ = librosa.load(path, sr=sr, mono=mono)
+    audio = librosa.util.normalize(audio)
+    audio, _ = librosa.effects.trim(audio, top_db=60, ref=np.max, frame_length=FRAME_LENGTH, hop_length=HOP_LENGTH)
     return audio
 
 
-def get_spectrum(audio):
-    spectrum = stft(audio, n_fft=FRAME_LENGTH, hop_length=HOP_LENGTH, center=False)
-    return np.absolute(spectrum)
+def get_stft(audio):
+    return librosa.stft(audio, n_fft=FRAME_LENGTH, hop_length=HOP_LENGTH)
 
 
-class AudioDataset(Dataset):
-    def __init__(self, file_paths=None, example_length=3, transform=None, target_transform=None):
-        super(AudioDataset, self).__init__()
-        self.transform = transform
-        self.target_transform = target_transform
+class SoundDataset(Dataset):
+    def __init__(self, audio_directory=DEFAULT_DIR, example_length=15, stft_hop_length=5, normalize=True):
+        super(SoundDataset, self).__init__()
 
-        if file_paths is None:
-            file_paths = recursive_file_paths(DEFAULT_DIR)
-        with Pool(8) as p:
-            results = list(p.map(load_audio_file, file_paths))
+        self._example_length = example_length
+        self._stft_hop_length = stft_hop_length
 
-        self.audio = torch.from_numpy(np.concatenate(results))
+        file_paths = librosa.util.find_files(audio_directory)
+        audio_list = do_multiprocess(load_audio_file, file_paths)
+        stft_list = do_multiprocess(get_stft, audio_list)
 
-        self.example_length = example_length
+        complex_stfts = np.concatenate(stft_list, axis=1)
+        shape = complex_stfts.shape
+        tmp = np.zeros((*shape, 2), dtype='float32')
+        tmp[:, :, 0] = complex_stfts.real
+        tmp[:, :, 1] = complex_stfts.imag
 
-    def update_strided_views(self):
-        num_samples = (self.example_length - 1) * HOP_LENGTH + FRAME_LENGTH
-        num_examples = (self.audio.shape[0] - num_samples) // HOP_LENGTH + 1
-        self.examples = self.audio.as_strided((num_examples, 1, num_samples),
-                                              (HOP_LENGTH, 1, 1))
+        self.stfts = torch.from_numpy(tmp)
+        self.spectra = torch.from_numpy(np.abs(complex_stfts))
 
-    @property
-    def example_length(self):
-        return self._example_length
+        if normalize:
+            self.stfts /= self.spectra.max()
+            self.spectra /= self.spectra.max()
 
-    @example_length.setter
-    def example_length(self, value):
-        self._example_length = value
-        self.update_strided_views()
+        self.complex_examples = None
+        self.absoulte_examples = None
 
-    def __len__(self):
-        return self.examples.shape[0]
+        self.update_strided()
 
-    def __getitem__(self, index):
-        result = self.examples[index]
-        if self.transform is not None:
-            example = self.transform(result)
-        else:
-            example = result
+    def update_strided(self):
+        stride = self.stfts.stride()
+        shape = self.stfts.shape
+        n_examples = (shape[1] - self.example_length) // self.stft_hop_length + 1
+        self.complex_examples = self.stfts.as_strided(
+            (n_examples, shape[0], self.example_length, shape[2]),
+            (stride[1] * self.stft_hop_length, stride[0], stride[1], stride[2]))
 
-        if self.target_transform is not None:
-            target = self.target_transform(result)
-        else:
-            target = result
-        return example, target
-
-
-class SpectrumDataset(Dataset):
-    def __init__(self, file_paths=None, example_length=3, transform=None, target_transform=None):
-        super(SpectrumDataset, self).__init__()
-        self.transform = transform
-        self.target_transform = target_transform
-
-        if file_paths is None:
-            file_paths = recursive_file_paths(DEFAULT_DIR)
-        with Pool(8) as p:
-            audio = list(p.map(load_audio_file, file_paths))
-
-        with Pool(8) as p:
-            spectrum = list(p.map(get_spectrum, audio))
-
-        self.audio = torch.from_numpy(np.concatenate(audio))
-        self.spectrum = torch.from_numpy(np.concatenate(spectrum, 1))
-        self.mins = self.spectrum.min()
-        self.maxes = self.spectrum.max()
-        self.spectrum = (self.spectrum - self.mins) / (self.maxes - self.mins)
-
-        self.example_length = example_length
-
-    def denormalize(self, x):
-        return x * (self.maxes - self.mins) + self.mins
-
-    def update_strided_views(self):
-        num_samples = (self.example_length - 1) * HOP_LENGTH + FRAME_LENGTH
-        num_examples = (self.audio.shape[0] - num_samples) // HOP_LENGTH + 1
-
-        strides = self.spectrum.stride()
-        self.examples = self.spectrum.as_strided((num_examples, FRAME_LENGTH // 2 + 1, self.example_length),
-                                                 (strides[1], strides[0], strides[1]))
-
-        self.labels = self.audio.as_strided((num_examples, 1, num_samples),
-                                            (HOP_LENGTH, 1, 1))
+        stride = self.spectra.stride()
+        shape = self.spectra.shape
+        self.absoulte_examples = self.spectra.as_strided(
+            (n_examples, shape[0], self.example_length),
+            (stride[1] * self.stft_hop_length, stride[0], stride[1]))
 
     @property
     def example_length(self):
@@ -149,21 +81,40 @@ class SpectrumDataset(Dataset):
     @example_length.setter
     def example_length(self, value):
         self._example_length = value
-        self.update_strided_views()
+        self.update_strided()
+
+    @property
+    def stft_hop_length(self):
+        return self._stft_hop_length
+
+    @stft_hop_length.setter
+    def stft_hop_length(self, value):
+        self._stft_hop_length = value
+        self.update_strided()
 
     def __len__(self):
-        return self.examples.shape[0]
+        return self.complex_examples.shape[0]
 
     def __getitem__(self, index):
-        example = self.examples[index]
-        label = self.labels[index]
-        if self.transform is not None:
-            example = self.transform(example)
-        else:
-            example = example
+        return self.complex_examples[index], self.absoulte_examples[index]
 
-        if self.target_transform is not None:
-            target = self.target_transform(label)
-        else:
-            target = label
-        return example, target
+
+class AtemporalDataset(Dataset):
+    def __init__(self, audio_directory=DEFAULT_DIR, normalize=True):
+        super(AtemporalDataset, self).__init__()
+
+        file_paths = librosa.util.find_files(audio_directory)
+        audio_list = do_multiprocess(load_audio_file, file_paths)
+        stft_list = do_multiprocess(get_stft, audio_list)
+
+        complex_stfts = np.concatenate(stft_list, axis=1)
+        self.spectra = torch.from_numpy(np.abs(complex_stfts)).transpose(1, 0).contiguous()
+
+        if normalize:
+            self.spectra /= self.spectra.max()
+
+    def __len__(self):
+        return self.spectra.shape[0]
+
+    def __getitem__(self, index):
+        return self.spectra[index]
